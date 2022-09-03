06dde6196782647e181885057c841bc4e222e5f6
0xpeIpeI <hokumaru12@gmail.com>
2022-09-03 13:58:03 +0900

    modify change file name && add webvtt struct

--- cmd/webvtt.go
+++ cmd/webvtt.go
@@ -1,1 +1,14 @@
 package cmd
+
+type WebVtt struct {
+	VttElements []*VTTElement `json:"vtt_elements"`
+}
+
+type VTTElement struct {
+	StartTime string `json:"start_time"`
+}
+
+//AppendVttElement append VTTElement to WebVtt
+func (wv *WebVtt) AppendVttElement(vtt *VTTElement) {
+	wv.VttElements = append(wv.VttElements, vtt)
+}
