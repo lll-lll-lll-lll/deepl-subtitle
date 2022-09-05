870a4f5f9e0a85b38bfb545b858b48d3d3ae04f0
0xpeIpeI <hokumaru12@gmail.com>
2022-09-05 18:04:17 +0900

    add UnifyTextByTerminalPoint test

--- cmd/segment_test.go
+++ cmd/segment_test.go
@@ -19,28 +19,28 @@ func TestTextSegment(t *testing.T) {
 		}
 		t.Log(got)
 	})
-	t.Run("", func(t *testing.T) {
-		wantpr := token[:19] // you need to know it
-		wantba := token[20:] // ♪ I know ♪
-		wantterminal := "?"
-		locs := SearchTerminalTokenRegexp(token)
-		fmt.Print(locs)
-		_, pr, ba, ter, flag := SplitByCommaAndQuestion(token)
-		if flag {
-			t.Log("no word in text. `?` or `.`")
-		} else {
-			if pr != wantpr {
-				t.Errorf("got %s want %s", pr, wantpr)
-			}
-			if ba != wantba {
-				t.Errorf("got %s want %s", ba, wantba)
-			}
+	// t.Run("", func(t *testing.T) {
+	// 	wantpr := token[:19] // you need to know it
+	// 	wantba := token[20:] // ♪ I know ♪
+	// 	wantterminal := "?"
+	// 	locs := SearchTerminalTokenRegexp(token)
+	// 	fmt.Print(locs)
+	// 	_, pr, ba, ter, flag := SplitByCommaAndQuestion(token)
+	// 	if flag {
+	// 		t.Log("no word in text. `?` or `.`")
+	// 	} else {
+	// 		if pr != wantpr {
+	// 			t.Errorf("got %s want %s", pr, wantpr)
+	// 		}
+	// 		if ba != wantba {
+	// 			t.Errorf("got %s want %s", ba, wantba)
+	// 		}
 
-			if ter != wantterminal {
-				t.Errorf("got %s want %s", "?", wantterminal)
-			}
-		}
-	})
+	// 		if ter != wantterminal {
+	// 			t.Errorf("got %s want %s", "?", wantterminal)
+	// 		}
+	// 	}
+	// })
 
 	t.Run("`UnifyTextByTerminalPoint` method test ", func(t *testing.T) {
 		f, err := ReadFile(filename)
@@ -50,10 +50,27 @@ func TestTextSegment(t *testing.T) {
 		webVtt := NewWebVtt(f)
 		webVtt.ScanLines(ScanTimeLineSplitFunc)
 		w := UnifyTextByTerminalPoint(webVtt)
-		got := w.VttElements[1].Text
-		want := "- Yo what is going on guys,welcome back to the channel."
-		fmt.Println(got)
-		fmt.Println(want)
+		got := w.VttElements[0].Text
+		want := "- Yo what is going on guys, welcome back to the channel."
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+		//PrintlnJson(w.VttElements)
+	})
+	t.Run("`DeleteEmptyTextVTTElementStruct` method test", func(t *testing.T) {
+		f, err := ReadFile(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		webVtt := NewWebVtt(f)
+		webVtt.ScanLines(ScanTimeLineSplitFunc)
+		w := UnifyTextByTerminalPoint(webVtt)
+		w = DeleteVTTElementStructOfEmptyText(w)
+		got := len(w.VttElements)
+		want := 3
+		if got != want {
+			t.Errorf("got %d, want %d ", got, want)
+		}
 		PrintlnJson(w.VttElements)
 	})
 }
