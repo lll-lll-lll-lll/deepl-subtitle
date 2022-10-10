9107b5c0351615cc31e8bd6130350b216852abc3
0xpeIpeI <hokumaru12@gmail.com>
2022-10-10 19:01:26 +0900

    modify: pkg

--- regexp.go
+++ regexp.go
@@ -1,47 +1,47 @@
-package deeplyoutubesubtitle
+package main
 
 import (
 	"regexp"
 )
 
-//CheckStartOrEndTimeFlag Check if the first 2 characters are 0~9 of int
+// CheckStartOrEndTimeFlag Check if the first 2 characters are 0~9 of int
 func CheckStartOrEndTimeFlag(token string) bool {
 	return CheckRegexp(`^[0-9]+`, token)
 }
 
-//CheckSeparatorFlag // Check if the first character is `-->`
+// CheckSeparatorFlag // Check if the first character is `-->`
 func CheckSeparatorFlag(token string) bool {
 	return CheckRegexp(`^-->`, token)
 }
 
-//CheckPositionFlag Check if the first character is `position:...`
+// CheckPositionFlag Check if the first character is `position:...`
 func CheckPositionFlag(token string) bool {
 	return CheckRegexp(`^position:[0-9]+%`, token)
 }
 
-//CheckLineFlag Check if the first character is `line:...`
+// CheckLineFlag Check if the first character is `line:...`
 func CheckLineFlag(token string) bool {
 	return CheckRegexp(`^line:[0-9]+%`, token)
 }
 
-//CheckTerminalFlag Check if the en character is `.` or `?`
+// CheckTerminalFlag Check if the en character is `.` or `?`
 func CheckTerminalFlag(token string) bool {
 	return CheckRegexp(`.`, token) || CheckRegexp(`?`, token)
 }
 
-//CheckRegexp Pattern detection of regular expression things method
+// CheckRegexp Pattern detection of regular expression things method
 func CheckRegexp(pattern, str string) bool {
 	return regexp.MustCompile(pattern).Match([]byte(str))
 }
 
-//SearchTerminalTokenRegexp 「.」か「?」を含んでいるか
+// SearchTerminalTokenRegexp 「.」か「?」を含んでいるか
 func SearchTerminalTokenRegexp(token string) []int {
 	r, _ := regexp.Compile("[.?]")
 	locs := r.FindStringIndex(token)
 	return locs
 }
 
-//CheckHeaderFlag headerならtrue
+// CheckHeaderFlag headerならtrue
 func CheckHeaderFlag(token string) bool {
 	return CheckRegexp(`WEBVTT`, token) || CheckRegexp(`Kind: captions`, token)
 }
--- segment_test.go
+++ segment_test.go
@@ -1,4 +1,4 @@
-package deeplyoutubesubtitle
+package main
 
 import (
 	"log"
