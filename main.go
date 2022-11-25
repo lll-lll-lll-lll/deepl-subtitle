ad141af1a66e917cabec53f1c9f6242644357f3b
ShunpeIII <63499912+lll-lll-lll-lll@users.noreply.github.com>
2022-11-25 07:53:26 +0000

    import

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"os"
+
+	"github.com/lll-lll-lll-lll/deepl-subtitle/cli"
 )
 
 func main() {
-	cli := cli.NewCLI(os.Stdout, os.Stderr)
+	cli := cli.New(os.Stdout, os.Stderr)
 	os.Exit(cli.Run(os.Args))
 }
